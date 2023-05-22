83bfa5bc5d977807d768e3a6bb5e96ff959406f2
Juancarranza02 <79293560+Juancarranza02@users.noreply.github.com>
2023-05-22 17:05:03 -0600

    Add files via upload

---
+++ Examen-Unidad-II-de-PIGU-2023/src/examen/unidad/ii/de/pigu/pkg2023/Alumno.java
@@ -0,0 +1,13 @@
+
+package examen.unidad.ii.de.pigu.pkg2023;
+
+public class Alumno {
+    
+    public String Carranza63841(){
+        String nombre = "JUAN A.";
+        String apellido = "CARRANZA MADAGAN";
+        String Carrera = "";
+        return "Mi nombre es "+nombre + " "+apellido +" y estudio "+Carrera ;
+    }
+    
+}
---
+++ Examen-Unidad-II-de-PIGU-2023/src/examen/unidad/ii/de/pigu/pkg2023/ExamenUnidadIIDePIGU2023.java
@@ -0,0 +1,14 @@
+
+package examen.unidad.ii.de.pigu.pkg2023;
+
+public class ExamenUnidadIIDePIGU2023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Alumno alumno1 = new Alumno();
+        System.out.println("Respuesta de Alumno "+alumno1.Carranza63841());
+    }
+    
+}
